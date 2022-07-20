1b541f8b8200c503e55645818f668d023a5f4a66
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2022-07-20 00:15:51 -0400

    fix globalData

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Edward Boroda';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Nostalgie';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Copyright © 2022 Edward Boroda. All rights reserved.';
 
   return {
     name,
